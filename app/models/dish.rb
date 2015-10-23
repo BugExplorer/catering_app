1792bfc67708c53986ff6a4b16f19573eb6ce949
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2015-10-23 10:46:00 +0300

    Dish belongs to category

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,5 +2,5 @@ class Dish < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 45 }
   validates :price, presence: true
 
-  # belongs_to :category
+  belongs_to :category
 end
