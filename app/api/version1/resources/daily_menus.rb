eff4e17e42d2a3a267ab34b7ecfbc7cbb46e89c0
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2015-11-09 21:32:05 +0200

    Move logic from resource to a separate model.

--- app/api/version1/resources/daily_menus.rb
+++ app/api/version1/resources/daily_menus.rb
@@ -6,7 +6,7 @@ module API
       version 'v1', using: :path
 
       resource :daily_menus do
-        desc 'Returns daily menus, with categories that has dishes', headers: {
+        desc '# Returns daily menus, where each day has categorized dishes', headers: {
           'X-Auth-Token' => {
             description: 'User token',
             required: true
@@ -14,27 +14,8 @@ module API
         }
 
         get '/' do
-          menus = DailyMenu.all.as_json(except: [:created_at, :updated_at])
-          menus.each do |resp|
-            categories = Category.all
-                         .as_json(except: [:created_at, :updated_at])
-
-            dishes = Dish.find(resp['dish_ids'])
-            grouped_dishes = dishes.group_by { |d| d['category_id'] }
-
-            # Create categories array node for each DailyMenu
-            resp['categories'] = categories
-            resp['categories'].each do |category|
-              category['dishes'] =
-                grouped_dishes[category['id']]
-                .as_json(except: [:sort_order, :created_at, :updated_at])
-            end
-
-            # Delete empty categories
-            resp['categories'].reject! { |cat| !cat['dishes'] }
-            # There are no sense to store dish_ids
-            resp.delete('dish_ids')
-          end
+          # get FormModelsStorage
+          FormModelsStorage.get_data
         end
       end
     end
---
+++ app/models/form_models_storage.rb
@@ -0,0 +1,29 @@
+class FormModelsStorage
+  # Returns DailyMenus, where each day has categorized dishes
+  def self.get_data
+    menus = DailyMenu.all.as_json(except: [:created_at, :updated_at])
+
+    menus.each do |menu|
+      categories = Category.all
+                   .as_json(except: [:created_at, :updated_at])
+
+      dishes = Dish.find(menu['dish_ids'])
+      grouped_dishes = dishes.group_by { |d| d['category_id'] }
+
+      # Create categories array node for each DailyMenu
+      menu['categories'] = categories
+      menu['categories'].each do |category|
+        category['dishes'] =
+          grouped_dishes[category['id']]
+          .as_json(except: [:sort_order, :created_at, :updated_at])
+      end
+
+      # Delete empty categories
+      menu['categories'].reject! { |cat| !cat['dishes'] }
+      # There are no sense to store dish_ids
+      menu.delete('dish_ids')
+    end
+
+    menus
+  end
+end
