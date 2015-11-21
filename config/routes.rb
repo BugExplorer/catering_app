442fb754d390de1f72ca85cd34cb3b2575503564
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2015-11-21 19:38:39 +0200

    ActiveAdmin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  mount API::Engine => "/api"
+  mount API::Engine => '/api'
 
-  devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  devise_for :users, controllers: { sessions: 'users/sessions',
+                                    registrations: 'users/registrations',
+                                    passwords: 'users/passwords'
+                                  }
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    authenticated :user do
+      root to: 'admin/dashboard#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root to: 'users/sessions#new', as: :unauthenticated_root
+    end
+  end
+  # root to: 'sessions#new'
+  root 'welcome#index'
 end
