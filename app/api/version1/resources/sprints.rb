4e024c6909d85600a5f27d8c97b4ae19999b9ae6
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2015-11-21 23:48:59 +0200

    Total price validation in daily rations creation

--- app/api/version1/resources/sprints.rb
+++ app/api/version1/resources/sprints.rb
@@ -35,34 +35,47 @@ module API
           authenticate_by_token!
 
           sprint = Sprint.find(params[:id])
+          user_id = current_user.id
           dishes = Dish.select(:id, :price).as_json
           # Convert dishes to the hash (:dish_id => :price)
           # To optimize amount of db requests
           dishes_hash = Hash[dishes.map(&:values).map(&:flatten)]
-          user_id = current_user.id
+
+          daily_menus = DailyMenu.select(:id, :max_total).as_json
+          # Convert daily menus to the hash (:id => :max_total)
+          daily_menus_hash = Hash[daily_menus.map(&:values).map(&:flatten)]
 
           daily_rations = []
           params[:days].each do |day|
+            total_price = 0
             # Get daily menu id from the hash
             daily_menu_id = day[0].to_i
             # Iterate through dish => quanity hash
-            daily_rations = day[1].map do |dish|
-              # Get dish id and his quanity from the dishes array
-              dish_id = dish[0]
-              dish_quantity = dish[1]
+            # Break if one of the total prices is bigger than day's
+            break unless day[1].each do |dish|
+              # Get dish id and its quanity from the dishes array
+              dish_id = dish[0].to_i
+              dish_quantity = dish[1].to_i
               # Get dish price from tha hash
               dish_price = dishes_hash[dish_id]
-              DailyRation.new(daily_menu_id: daily_menu_id,
-                              dish_id:       dish_id,
-                              price:         dish_price,
-                              quantity:      dish_quantity,
-                              user_id:       user_id,
-                              sprint_id:     sprint.id)
+              total_price += dish_price
+              # Check if total price is bigger than day limit
+              if total_price >= daily_menus_hash[daily_menu_id]
+                daily_rations = nil
+                break
+              else
+                daily_rations << DailyRation.new(daily_menu_id: daily_menu_id,
+                                                 dish_id:       dish_id,
+                                                 price:         dish_price,
+                                                 quantity:      dish_quantity,
+                                                 user_id:       user_id,
+                                                 sprint_id:     sprint.id)
+              end
             end
           end
 
           # Push that as one db request
-          DailyRation.import daily_rations, validate: true
+          DailyRation.import daily_rations, validate: true if daily_rations
         end
 
         desc 'Returns sprint by id', headers: {
